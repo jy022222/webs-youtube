f8a22337bab843736285eab1a2db807bf72de6b5
Juyoung <jy022222@naver.com>
2024-06-06 23:38:58 +0900

    페이지 SEO 작업

--- src/pages/Channel.jsx
+++ src/pages/Channel.jsx
@@ -1,9 +1,13 @@
-import React from 'react'
+import React from 'react';
+import Main from '../components/section/Main'
 
-const Channel = () => {
+const Home = () => {
   return (
-    <div>Channel</div>
+    <Main title="유튜브 채널" 
+    description="유튜브 채널페이지입니다.">
+      Cannel
+   </Main>
   )
 }
 
-export default Channel
+export default Home
--- src/pages/Port.jsx
+++ src/pages/Port.jsx
@@ -1,9 +1,14 @@
-import React from 'react'
+import React from 'react';
+import Main from '../components/section/Main'
 
-const Port = () => {
+const Home = () => {
   return (
-    <div>Port</div>
+    <Main 
+      title = "포트폴리오 사이트"
+      description="포트폴리오 사이트 튜토리얼 강의입니다.">
+      Port
+   </Main>
   )
 }
 
-export default Port
+export default Home
