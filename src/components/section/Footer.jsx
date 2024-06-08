d373b997884bb9e58de635e1f2fc68bcce480c2a
Juyoung <jy022222@naver.com>
2024-06-08 12:53:14 +0900

    메인 컨텐츠 작업

--- src/components/section/Footer.jsx
+++ src/components/section/Footer.jsx
@@ -2,7 +2,11 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <footer id='footer' role='contentinfo'>Footer</footer>
+    <footer id='footer' role='contentinfo'>
+        <a href="mailto:jy022222@naver.com" rel='noopenner noreferrer'>
+          jy022222@naver.com
+        </a>
+    </footer>
   )
 }
 
